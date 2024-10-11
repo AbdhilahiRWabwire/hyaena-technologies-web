567f7629d448184d0d6daf9ea39ae8a26dedaa5f
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-10-10 21:43:28 -0600

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:web/web.dart';
-import 'package:hyaena_technologies/html_elements.dart';
+import 'package:hyaena_technologies/create_elements.dart';
 
 void main() async {
-  division.text = 'Hyaena Technologies';
+  createDivision.text = 'Hyaena Technologies';
 }
