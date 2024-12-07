2efbf9d46f435c69d81d9cb38c655046ae604675
Abdhilahi Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-12-06 17:40:15 -0700

    Remove Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:web/web.dart';
-import 'package:hyaena_technologies/create_elements.dart';
 
 void main() async {
-  createDivision.text = 'Hyaena Technologies';
+  document.createElement('div');
 }
