bf89ea5ebdfd171434c6fbe79301d0511cd418b1
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-03-14 13:28:51 -0600

    Fix HashMaps

--- src/arguments/commands.rs
+++ src/arguments/commands.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 // Command Definition
 struct CommandArguments{
     name: String,
@@ -6,34 +8,28 @@ struct CommandArguments{
 }
 
 // Command Map
-pub fn command_map() -> (
-    String, 
-    CommandArguments, 
-    String, 
-    CommandArguments, 
-    String, 
-    CommandArguments
-) {
-    let command_arguments: (
-        String, 
-        CommandArguments, 
-        String, 
-        CommandArguments, 
-        String, 
-        CommandArguments
-    )  = (
+pub fn command_map() -> HashMap<String, CommandArguments> {
+    let command_arguments: HashMap<String, CommandArguments> = HashMap::new();
+
+    command_arguments.insert( 
         "exit".to_string(),
         CommandArguments {
             name: "exit".to_string(),
             description: "Exit Server".to_string(),
             event: succesful_exit(),
-        },
+        }
+    );
+
+    command_arguments.insert(
         "help".to_string(),
         CommandArguments {
             name: "help".to_string(),
             description: "Print List of Commands and Flags".to_string(),
             event: print_help(),
         },
+    );
+    
+    command_arguments.insert(
         "version".to_string(),
         CommandArguments {
             name: "version".to_string(),
--- src/arguments/flags.rs
+++ src/arguments/flags.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 // Flag Definition
 struct FlagArguments {
     name: String,
@@ -6,64 +8,55 @@ struct FlagArguments {
 }
 
 // Flag Map
-pub fn flag_map() -> (
-    String, 
-    FlagArguments, 
-    String, 
-    FlagArguments, 
-    String, 
-    FlagArguments, 
-    String, 
-    FlagArguments, 
-    String, 
-    FlagArguments, 
-    String, 
-    FlagArguments
-) {
-    let flag_arguments: (
-        String, 
-        FlagArguments, 
-        String, 
-        FlagArguments, 
-        String, 
-        FlagArguments, 
-        String, 
-        FlagArguments, 
-        String, 
-        FlagArguments, 
-        String, 
-        FlagArguments
-    ) = (
+pub fn flag_map() -> HashMap<String, FlagArguments> {
+    let flag_arguments: HashMap<String, FlagArguments> = HashMap::new();
+
+    flag_arguments.insert(
         "--exit".to_string(),
         FlagArguments {
             name: "exit".to_string(),
             description: "Exit Server".to_string(),
             event: succesful_exit(),
         },
+    );
+
+    flag_arguments.insert(
         "--e".to_string(),
         FlagArguments {
             name: "exit".to_string(),
             description: "Exit Server".to_string(),
             event: succesful_exit(),
         },
+    );
+
+    flag_arguments.insert(
         "--help".to_string(),
         FlagArguments {
             name: "help".to_string(),
             description: "Print List of Commands and Flags".to_string(),
             event: print_help(),
         },
+    );
+
+    flag_arguments.insert(
         "--h".to_string(),
         FlagArguments {
             name: "help".to_string(),
             description: "Print List of Commands and Flags".to_string(),
             event: print_help(),
         },
+    );
+
+    flag_arguments.insert(
         "--version".to_string(),
         FlagArguments {
             name: "version".to_string(),
             description: "Print Version Number".to_string(),
             event: print_version_number(),
         },
+    );
+    
+    flag_arguments.insert(
         "--v".to_string(),
         FlagArguments {
             name: "version".to_string(),
