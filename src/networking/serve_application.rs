fba3f52a5cfa690a5307efc2ccc029854796d9c2
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-03-26 18:02:56 -0600

    Remove Template

--- src/networking/serve_application.rs
+++ src/networking/serve_application.rs
@@ -19,33 +19,3 @@ use hyper::{
     service::service_fn
 };
 
-
-pub async fn web_service(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
-}
-
-pub async fn start_service() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    // We create a TcpListener and bind it to 127.0.0.1:3000
-    let listener = TcpListener::bind(addr).await?;
-
-    // We start a loop to continuously accept incoming connections
-    loop {
-        let (stream, _) = listener.accept().await?;
-
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
-        let io = stream;
-
-        // Spawn a tokio task to serve multiple connections concurrently
-            // Finally, we bind the incoming connection to our `hello` service
-            if let Err(err) = http1::Builder::new()
-                // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service_fn(hello))
-                .await
-            {
-                eprintln!("Error serving connection: {:?}", err);
-            }
-    }
-}
