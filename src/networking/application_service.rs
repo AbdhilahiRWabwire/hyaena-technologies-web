81488984c1025a9bd5ac092f60831d87c1b48579
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-04-02 03:02:51 -0600

    Fix Stream Address

--- src/networking/application_service.rs
+++ src/networking/application_service.rs
@@ -4,20 +4,14 @@
 #![allow(unused_variables)]
 
 use std::{
-    net::{
-        IpAddr, 
-        Ipv4Addr, 
-        SocketAddrV4, 
-        TcpListener, 
-        TcpStream
-    },
-    string::String
+    net::{IpAddr, Ipv4Addr, SocketAddrV4, TcpListener, TcpStream},
+    string::String,
 };
 
 pub fn web_service() -> () {
     let socket_address: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080);
-    let transmission_listener:TcpListener = TcpListener::bind(socket_address).unwrap(); 
-    let transmission_stream: TcpStream = TcpStream::connect(transmission_control_listener).unwrap();
+    let transmission_listener: TcpListener = TcpListener::bind(socket_address).unwrap();
+    let transmission_stream: TcpStream = TcpStream::connect(socket_address).unwrap();
 
-    return ()
+    return ();
 }
