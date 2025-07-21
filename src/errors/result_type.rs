f2ad8cc5dfc4cf5cfd289489bf67a6f64df22768
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-07-20 18:51:45 -0600

    Update Custom Result Type

--- src/errors/result_type.rs
+++ src/errors/result_type.rs
@@ -1,10 +1,32 @@
 #![allow(dead_code)]
 
-use std::primitive::bool;
+use std::{option::Option, primitive::bool};
 
 // Custom Result Type Definition
 pub struct Result<T> {
-    error: T,
+    error: Option<T>,
     successful: bool,
-    value: T,
+    value: Option<T>,
+}
+
+// Initialize a Result with a Validated Value
+fn okay<T>(val: Option<T>) -> Result<T> {
+    let result_value: Result<T> = Result {
+        error: None,
+        successful: true,
+        value: val,
+    };
+
+    return result_value;
+}
+
+// Initialize a Result with a Validated Error
+fn error<T>(err: Option<T>) -> Result<T> {
+    let result_error: Result<T> = Result {
+        error: err,
+        successful: false,
+        value: None,
+    };
+
+    return result_error;
 }
